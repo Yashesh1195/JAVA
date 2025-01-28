c55eac1808e4049d4cc6d4c9e3ff4f19536acc7e
Yashesh Mehta <155947815+Yashesh1195@users.noreply.github.com>
2025-01-28 23:04:42 +0530

    Add files via upload

---
+++ LecA/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World with Java!!"); //next line
+
+        System.out.print("Hello World with Java!!\nHello World with Java!!\n");//next line
+
+        System.out.print("Hello World with Java!!");
+    }
+}
---
+++ LecA/PracticeA.java
@@ -0,0 +1,8 @@
+public class PracticeA {
+    public static void main(String[] args){
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+}
