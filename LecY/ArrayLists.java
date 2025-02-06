52970dc837bb2cd8337a744dfddce1a485d13f3c
Yashesh Mehta <155947815+Yashesh1195@users.noreply.github.com>
2025-02-06 23:13:09 +0530

    Add files via upload

---
+++ LecY/ArrayLists.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.Collections;
+// import java.util.*;
+
+class ArrayLists {
+    public static void main(String args[]) {
+        // Integer | Float | String | Boolean
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        // ArrayList<String> list2 = new ArrayList<String>();
+        // ArrayList<Boolean> list3 = new ArrayList<Boolean>();
+
+        //add elements
+
+        list.add(0);
+        list.add(2);
+        list.add(3);
+
+        System.out.println(list);
+
+        //get elements
+
+        int element = list.get(0);
+        System.out.println(element);
+
+        //add element in between
+
+        list.add(1, 1);
+        System.out.println(list);
+
+        //set element
+        list.set(0, 5);
+        System.out.println(list);
+
+        //delete element
+        list.remove(3);
+        System.out.println(list);
+
+        //size of arraylist
+        int size = list.size();
+        System.out.println(size);
+
+        //Loops on list
+        for(int i=0; i<list.size(); i++){
+            System.out.println(list.get(i));
+        }
+        System.out.println();
+
+        //Sorting the list
+        Collections.sort(list);
+        System.out.println(list);
+    }
+}
