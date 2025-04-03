20bf4e1571872c3f2e60bed139104fd421daaee5
Yashesh Mehta <155947815+Yashesh1195@users.noreply.github.com>
2025-04-03 06:20:52 +0530

    Delete First_JavaFX_Project directory

--- First_JavaFX_Project/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.first_javafx_project {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.first_javafx_project to javafx.fxml;
-    exports com.example.first_javafx_project;
-}
