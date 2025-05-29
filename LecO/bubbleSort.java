63ce0d5248e3abd83ad464b5c2092ac2e16f4dfb
Yashesh Mehta <155947815+Yashesh1195@users.noreply.github.com>
2025-05-29 12:29:23 +0530

    Add files via upload

---
+++ LecO/bubbleSort.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class bubbleSort {
+    public static void printArray(int arr[]) {
+        for(int i = 0; i<arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //denoting the size of the array
+        System.out.print("Enter the size of array: ");
+        int size = sc.nextInt();
+
+        //Getting elements of the array
+        int arr[] = new int[size];
+        System.out.print("Enter elements of array: ");
+        for(int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        //time complexity = O(n^2)
+        //bubble sort
+        for(int i = 0; i < size-1; i++) { //n-1
+            for(int j = 0; j < size-i-1; j++) {
+                if(arr[j] > arr[j+1]) {
+                    //swap
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                }
+            }
+        }
+
+        printArray(arr);
+    }
+}
