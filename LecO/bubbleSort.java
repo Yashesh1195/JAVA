213635381602d3ffa04a09697cc88ac722dd5658
Yashesh Mehta <155947815+Yashesh1195@users.noreply.github.com>
2025-05-30 10:46:55 +0530

    Add files via upload

--- LecO/bubbleSort.java
+++ LecO/bubbleSort.java
@@ -34,5 +34,7 @@ class bubbleSort {
         }
 
         printArray(arr);
+
+        sc.close();
     }
 }
--- LecO/insertionSort.java
+++ LecO/insertionSort.java
@@ -22,6 +22,17 @@ public class insertionSort {
 
         //time complexity = O(n^2)
         //insertion sort
+        for(int i = 1; i < arr.length; i++) {
+            int current = arr[i];
+            int j = i - 1;
+            while(j>=0 && current < arr[j]) {
+                arr[j+1] = arr[j];
+                j--;
+            }
+
+            //placement
+            arr[j+1] = current;
+        }
 
         printArray(arr);
 
