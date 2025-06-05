6574cddececce099a61b79bff5f2916da46442fc
Yashesh Mehta <155947815+Yashesh1195@users.noreply.github.com>
2025-06-05 20:44:30 +0530

    Add files via upload

--- LecQ/Recursion9.java
+++ LecQ/Recursion9.java
@@ -11,9 +11,26 @@
 //  9 -> yz
 //  )
 
+// Time Complexity - O(4^n)
+
 public class Recursion9 {
-    public static 
+    public static String[] keypad = {".", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    
+    public static void printComb(String str, int index, String combination) {
+        if(index == str.length()) {
+            System.out.println(combination);
+            return;
+        }
+        char currChar = str.charAt(index);
+        String mapping = keypad[currChar - '0'];
+
+        for(int i=0; i<mapping.length(); i++) {
+            printComb(str, index+1, combination+mapping.charAt(i));
+        }
+    }
     public static void main(String[] args) {
-        
+        String str = "2";
+        // String str = "327";
+        printComb(str, 0, "");
     }
 }
