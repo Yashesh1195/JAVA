c901266e54478aa834a5ce49badde90b22e2418a
Yashesh Mehta <155947815+Yashesh1195@users.noreply.github.com>
2025-06-05 20:12:51 +0530

    Add files via upload

--- LecQ/Recursion6.java
+++ LecQ/Recursion6.java
@@ -1,3 +1,26 @@
+// Remove duplicates in a string.
+// Time Complexity - O(n)
+
 public class Recursion6 {
-    
+    public static boolean[] map = new boolean[26];
+
+    public static void removeDuplicates(String str, int index, String newString) {
+        if(index == str.length()) {
+            System.out.println(newString);
+            return;
+        }
+        char currChar = str.charAt(index);
+        if(map[currChar - 'a'] == true) {
+            removeDuplicates(str, index+1, newString);
+        }
+        else {
+            newString += currChar;
+            map[currChar - 'a'] = true;
+            removeDuplicates(str, index+1, newString);
+        }
+    }
+    public static void main(String[] args) {
+        String str = "yashesh";
+        removeDuplicates(str, 0, "");
+    }    
 }
