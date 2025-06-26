2d5056c5e728607fa222f4d59381ec1b90d87312
Yashesh Mehta <155947815+Yashesh1195@users.noreply.github.com>
2025-06-26 17:31:43 +0530

    Add files via upload

--- LecU - Sorting/QuickSort.java
+++ LecU - Sorting/QuickSort.java
@@ -1,5 +1,54 @@
+// It can be implemented using Pivot and Partitioning Approach
+// It can be solved by considering any of the 4 elements as pivot elements
+// 1. random element
+// 2. median element
+// 3. 1st element
+// 4. Last element
+
+// Time Complexity :- Worst : O(n^2)
+//                    Average : O(nlogn)
+
 public class QuickSort {
+
+    public static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low-1;
+
+        for(int j=low; j<high; j++) {
+            if(arr[j] < pivot) {
+                i++;
+
+                // swap
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        i++;
+        int temp = arr[i];
+        arr[i] = pivot;
+        arr[high] = temp;
+        return i; // pivot index
+    }
+
+    public static void quickSort(int[] arr, int low, int high) {
+        if(low < high) {
+            int pivotIdx = partition(arr, low, high);
+
+            quickSort(arr, low, pivotIdx-1);
+            quickSort(arr, pivotIdx+1, high);
+        }
+    }
     public static void main(String[] args) {
+        int[] arr = {6, 3, 9, 5, 2, 8};
+        int n = arr.length;
         
+        quickSort(arr, 0, n-1);
+
+        // print
+        for(int i=0; i<n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
     }
 }
