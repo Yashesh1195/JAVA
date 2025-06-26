3b5c0993f722de91f14306a0016dd85238d423e6
Yashesh Mehta <155947815+Yashesh1195@users.noreply.github.com>
2025-06-26 17:16:19 +0530

    Add files via upload

---
+++ LecV - OOPJ/OOPS3.java
@@ -0,0 +1,45 @@
+//Inheritance :- Funtion Overriding
+
+// // Base/Parent Class
+// class Shape {
+//     String color;
+// }
+
+// // Sub/Child Class
+// class Triangle extends Shape {
+
+// }
+
+class Shape {
+    public void area() {
+        System.out.println("display area");
+    }
+}
+
+class Triangle extends Shape {
+    public void area(int l, int h) {
+        System.out.println(1/2*l*h);
+    }
+}
+
+//Multi-Level Inheritance
+// class EquilateralTriangle extends Triangle {
+//     public void area(int l, int h) {
+//         System.out.println(1/2*l*h);
+//     }
+// }
+
+//Hierarchical Inheritance
+class Circle extends Shape {
+    public void area(int r) {
+        System.out.println((3.14) * r * r);
+    }
+}
+
+public class OOPS3 {
+    public static void main(String args[]) {
+        
+
+
+    }
+}
---
+++ LecV - OOPJ/bank/Bank.java
@@ -0,0 +1,26 @@
+package bank;
+
+//Access Modifiers
+class Account {
+    public String name;
+    protected String email;
+    private String password;
+
+    //getters & seters
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String pass) {
+        this.password = pass;
+    }
+}
+public class Bank {
+    public static void main(String args[]) {
+        Account account1 = new Account();
+        account1.name = "Yashesh";
+        account1.email = "yashesh@gmail.com";
+        account1.setPassword("abcd");
+        System.out.println(account1.getPassword());
+    }
+}
