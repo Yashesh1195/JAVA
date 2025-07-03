816d60edd5233d735b10214c441d21bcaa5cfd25
Yashesh Mehta <155947815+Yashesh1195@users.noreply.github.com>
2025-07-03 07:32:13 +0530

    Add files via upload

---
+++ LecY - Stack/StackCF.java
@@ -0,0 +1,18 @@
+import java.util.*;
+// import java.util.Stack;
+
+public class StackCF {
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        s.push(5);
+
+        while(!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
---
+++ LecY - Stack/StackProblem1.java
@@ -0,0 +1,29 @@
+// Push at the bottom of the stack
+
+import java.util.Stack;
+
+public class StackProblem1 {
+    public static void pushAtBottom(int data, Stack<Integer> s) {
+        if(s.isEmpty()) {
+            s.push(data);
+            return;
+        }
+        int top = s.pop();
+        pushAtBottom(data, s);
+        s.push(top);
+    }
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+
+        pushAtBottom(5, s);
+
+        while(!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
